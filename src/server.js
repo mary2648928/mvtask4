ef41f412d2fb5e7642089441de3b739993af2d1c
mary2648928 <mvdruzhinina@gmail.com>
2022-08-02 18:54:21 +0300

    1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from "./App";
-import express from 'express';
+//import app from "./App";
+const app = express();
 const path = require("path")
 
 app.use(express.static(path.join(__dirname, "client", "build")))
@@ -8,3 +8,43 @@ app.get("*", (req, res) => {
 });
 const port = process.env.PORT || 5000;
 app.listen(port);
+
+
+const express = require("express");
+
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://admin:task4@task4.flmjrnj.mongodb.net/?retryWrites=true&w=majority";
+app.use(express.static("public"));
+app.get("/api/movie", async function (req, res) {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  try {
+    await client.connect();
+    const database = client.db('users');
+    const collection = database.collection('users');
+    const query = { status: { $exists: true } };
+    const cursor = await collection.aggregate([
+      { $match: query },
+      { $sample: { size: 1 } },
+      { $project:
+          {
+            title: 1
+            //fullplot: 1,
+            //poster: 1
+          }
+      }
+    ]);
+    const movie = await cursor.next();
+    return res.json(movie);
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+});
+
+// start the server listening for requests
+app.listen(process.env.PORT || 8000,
+  () => console.log("Server is running..."));
