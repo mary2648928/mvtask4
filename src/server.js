81cdadb714f3da3dbf4f514bae458b87ceca70e4
mary2648928 <mvdruzhinina@gmail.com>
2022-08-02 11:36:49 +0300

    1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from "./App";
 import express from  'express';
-const path = require("path")
+import mongodb from 'mongodb';
+
+/*const path = require("path")
 const port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, "client", "build")))
 app.get("*", (req, res) => {
@@ -8,3 +10,14 @@ app.get("*", (req, res) => {
 });
 require("dotenv").config();
 app.listen(port)
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:<password>@task4.flmjrnj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
